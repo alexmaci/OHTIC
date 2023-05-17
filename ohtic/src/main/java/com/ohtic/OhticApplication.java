1de997cec2cfa6c61c2b0b0b4d8ef7a0eb577ce6
alexmaci <alexmaci96@gmail.com>
2023-05-17 19:02:42 +0200

    Configuracion del proyecto: Gitignore + docker compose + proyecto de spring

---
+++ ohtic/src/main/java/com/ohtic/OhticApplication.java
@@ -0,0 +1,13 @@
+package com.ohtic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OhticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OhticApplication.class, args);
+	}
+
+}
---
+++ ohtic/src/test/java/com/ohtic/OhticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohtic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OhticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
